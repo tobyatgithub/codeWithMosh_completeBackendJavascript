fe0f0bbe5407aae6640d219f6df1c2f229b85dac
tobyatgithub <toby.fangyuan@gmail.com>
2021-10-12 20:48:13 -0700

    validate user input (post) with Joi.

--- part3_nodejs/express-demo/index.js
+++ part3_nodejs/express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require("joi");
 const express = require("express");
+
 const app = express();
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "python" },
@@ -12,6 +15,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   res.status(400).send("Name is required with length >= 3 char.");
+  //   return;
+  // }
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(courses);
 });
 
